598e6e7a3ad46dbf1c571e08b5c8d4965a72a8bf
Afia-Lydia <you@example.com>
2020-07-15 19:01:07 -0400

    mvp for mod3

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,7 +2,7 @@
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
-
+​
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
@@ -14,48 +14,59 @@ class BSTNode:
         self.value = value
         self.left = None
         self.right = None
-
     # Insert the given value into the tree
     def insert(self, value):
-        pass
-
+        # Case 1: value is less than self.value
+        if value < self.value:
+            # If there is no left child, insert value here
+            if self.left is None:
+               self.left = BSTNode(value)
+            else:
+                # Repeat the process on left subtree
+                self.left.insert(value)
+        # Case 2: value is greater than or equal self.value
+        elif value >= self.value:
+            # If there is no right child, insert value here
+            if self.right is None:
+               self.right = BSTNode(value)
+            else:
+                # Repeat the process on right subtree
+                self.right.insert(value)
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
-
+        # Case 1: self.value is equal to the target
+        if self.value == target:
+            return True
+        # Case 2: target is less than self.value 
+        if target < self.value:
+            # if self.left is None, it isn't in the tree
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        # Case 3: otherwise
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
     # Return the maximum value found in the tree
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
     def get_max(self):
-        pass
-
-    # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        pass
-
-    # Part 2 -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
-
-    # Stretch Goals -------------------------
-    # Note: Research may be required
-
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
-
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+        # forget about the left subtree
+        # iterate through the nodes using a loop construct
+        if not self: 
+            return None
+        
+        if not self.right:
+            return self.value 
+        
+        return self.right.get_max()
--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +15,34 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
 class Queue:
     def __init__(self):
         self.size = 0
         # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
+
+
+
 
     def enqueue(self, value):
-        pass
+        # adds item to the back (tail) of the queue
+        self.size += 1
+        # calling function add to tail to add item to the back of the queue
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # remove and return and item in front of the queue
+        # if there are items
+        if self.size > 0:
+            # we are reducing the size by one
+            self.size -= 1
+            # removing from head and returning that item
+            return self.storage.remove_head()
+
+    def len(self):
+        # returns the number of items in the queue
+        return self.size
